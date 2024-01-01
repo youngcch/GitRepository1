7bd1fc45719d0dad381e916eb557c8056d6c701b
youngcch <cyc8649@gmail.com>
2024-01-02 00:36:00 +0900

    Test git Action2

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -17,6 +17,6 @@ public class HelloApplication {
 class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello, Ci/Cd Test Success";
+		return "Hello, Ci/Cd Test Success2";
 	}
 }
